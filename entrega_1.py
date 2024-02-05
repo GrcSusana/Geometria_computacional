f6c5c12fad1bfb1b21e672ede6d1d3cbb9da12fb
GrcSusana <94780515+GrcSusana@users.noreply.github.com>
2024-02-05 19:46:02 +0100

    Update entrega_1.py

--- entrega_1.py
+++ entrega_1.py
@@ -40,6 +40,8 @@ distr_es = pd.DataFrame({'states': tab_es_states, 'probab': tab_es_probab })
 distr_es = distr_es.sort_values(by='probab', ascending=True)
 distr_es.index=np.arange(0,len(tab_es_states))
 
+
+"""
 ##### Para obtener una rama, fusionamos los dos states con menor frecuencia
 distr = distr_en
 ''.join(distr['states'][[0,1]])
@@ -58,6 +60,8 @@ distr.index=np.arange(0,len(states))
 
 #Creamos un diccionario
 branch = {'distr':distr, 'codigo':codigo}
+"""
+
 
 ## Ahora definimos una función que haga exáctamente lo mismo
 def huffman_branch(distr):
@@ -83,14 +87,24 @@ def huffman_tree(distr):
         tree = np.concatenate((tree, code), axis=None)
     return(tree)
  
-distr = distr_en 
-tree = huffman_tree(distr)
-tree[0].items()
-tree[0].values()
+ 
+tree_en = huffman_tree(distr_en)
+tree_en[0].items()
+tree_en[0].values()
 
 #Buscar cada estado dentro de cada uno de los dos items
-list(tree[0].items())[0][1] ## Esto proporciona un '0'
-list(tree[0].items())[1][1] ## Esto proporciona un '1'
+list(tree_en[0].items())[0][1] ## Esto proporciona un '0'
+list(tree_en[0].items())[1][1] ## Esto proporciona un '1'
+
+# Igual para la clave en español
+
+tree_es = huffman_tree(distr_es)
+tree_es[0].items()
+tree_es[0].values()
+
+#Buscar cada estado dentro de cada uno de los dos items
+list(tree_es[0].items())[0][1] ## Esto proporciona un '0'
+list(tree_es[0].items())[1][1] 
 
 
 # A partir de aqui ya no uso plantilla. Hay que recorrer el arbol que genera
@@ -104,6 +118,8 @@ list(tree[0].items())[1][1] ## Esto proporciona un '1'
 # Mejor recorremos el arbol desde el principio, pero hay que ir concatenando 
 # por delante
 
+# lo del codigo binario se puede coger la longitud truncada hacia arriba
+
 
 def caracteres_sin_repeticion(texto):
     l = []
@@ -113,12 +129,92 @@ def caracteres_sin_repeticion(texto):
     return l
 
 
-def recorrer_arbol_en(tree):
+def crea_diccionario_clave(tree):
     lista_caracteres = caracteres_sin_repeticion(en)
     lista_arbol = list(tree)
     diccionario = {}
-    for i in lista_caracteres:
+    for i in lista_arbol:
+        [k0, k1] = list(i.keys())
+        [v0, v1] = list(i.values())
+        if len(k0) == 1:
+            diccionario[k0] = str(v0)
+        if len(k0) > 1:
+            for j in k0:
+                diccionario[j] = str(v0) + diccionario[j]
+        if len(k1) == 1:
+            diccionario[k1] = str(v1)
+        if len(k1) > 1:
+            for j in k1:
+                diccionario[j] = str(v1) + diccionario[j]
+    return diccionario
+
+# Nos crea el diccionario para la clave en ingles
         
-        
+diccionario_en = crea_diccionario_clave(tree_en)
+
+diccionario_es = crea_diccionario_clave(tree_es)
 
 
+# Longitudes medias 
+
+def longitud_media(distr, diccionario):
+    lon = 0
+    l_caracteres = list(distr['states'])
+    l_prob = list(distr['probab']) 
+    i = 0
+    while i < len(l_caracteres):
+        lon += len(diccionario[l_caracteres[i]])*l_prob[i]
+        i += 1
+    return lon
+
+L_S_eng = longitud_media(distr_en, diccionario_en)
+
+L_S_esp = longitud_media(distr_es, diccionario_es)
+
+
+# Comprobar que se satisface el primer teorema de Shannon
+
+def entropia(distr):
+    ent = 0
+    l_prob = list(distr['probab'])
+    for i in l_prob:
+        ent += i*np.log2(i)
+    ent = ent * (-1)
+    return ent
+
+
+# Codificar y decodificar
+
+def codificar(palabra, diccionario):
+    r = ''
+    for i in palabra:
+        r = r + diccionario[i]
+    return r
+
+# Comprobar la eficiencia de longitud frente al codigo binario usual
+
+def codificar_en_binario(palabra):
+    # esta linea de codigo la he sacado de https://www.techiedelight.com/es/convert-string-to-binary-python/
+    lista = list(format(letra, 'b') for letra in bytearray(palabra, "utf-8"))
+    res = ''
+    for i in lista:
+        res = res + i
+    return res
+    
+
+def decodificar(palabra_codificada, diccionario):
+    r = ''
+    i = 0 
+    palabra = ''
+    keys = list(diccionario.keys())
+    vals = list(diccionario.values())
+    while i < len(palabra_codificada):
+        palabra = palabra + palabra_codificada[i]
+        if palabra in vals:
+            r = r + keys[vals.index(palabra)]
+            palabra = ''
+            i += 1
+        else:
+            i += 1
+    return r
+    
